a4bc317caaf59b5b036796326ec39295dd756514
iYogic2 <1678130538@qq.com>
2022-06-05 13:11:54 +0800

    feat: init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+ function isEmpty(param, regType = 'commonReg' ){
+   // 'fullReg' 全量匹配模式时，字符串形式的'null'和'undefined'也算是空
+   if( modeType && modeType === 'fullReg'){
+      return (param??'') === ''|| param==='null' || param === "undefined" ? true : false
+   }else{
+      // 'commonReg' 默认，常规匹配模式，字符串形式的'null'和'undefined'不算是空
+      return (param??'') === '' ? true : false
+   } 
+ }
